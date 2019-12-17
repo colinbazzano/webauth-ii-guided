869b7f017ffeddf99c397a1c41b837593edc683f
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-17 14:11:22 -0500

    Guided Project

--- api/server.js
+++ api/server.js
@@ -1,21 +1,50 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const sessions = require("express-session"); // install express-session
+const KnexSessionStore = require("connect-session-knex")(sessions); // THIS MUST BE *AFTER* THE SESSION ABOVE
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const knex = require("../database/dbConfig.js");
 
 const server = express();
+
+const sessionConfig = {
+  // session storage options
+  name: "chocolateChip", // default would be sid
+  secret: "keep it secret, keep it safe", // used for encryption (must be an environment variable)
+  saveUninitialized: true, // has implications with GDPR laws, must be set false in production.
+  resave: false,
+
+  // how to store the sessions
+  store: new KnexSessionStore({
+    // DO NOT FORGET THE new KEYWORD!!!
+    knex, // imported from dbConfig.js
+    createtable: true,
+    clearInterval: 1000 * 60 * 10, // clears the expired sessions
+    sidfieldname: "sid",
+    // optional below
+    tablename: "sessions"
+  }),
+  // cookie options
+  cookie: {
+    maxAge: 1000 * 60 * 10, // 10 minutes in milliseconds
+    secure: false, // if false, the cookie is sent over http, if true, only sent over https (in production, this should be set to true)
+    httpOnly: true // if true, JS cannot access the cookie
+  }
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(sessions(sessionConfig)); // THIS is what adds a req.session object
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,33 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+// const Users = require("../users/users-model.js");
 
+// module.exports = (req, res, next) => {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "Invalid Credentials" });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json({ message: "Ran into an unexpected error" });
+//       });
+//   } else {
+//     res.status(400).json({ message: "No credentials provided" });
+//   }
+// };
+
+// we check for req.session first because we would always want to see what's left to the dot when looking for something like req.session.user
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: "You shall not pass!" });
   }
 };
